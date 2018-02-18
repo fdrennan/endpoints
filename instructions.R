e36e1557f4490c61dcdd8f12b814f4070d1de10b
Freddy <rstudio@ip-172-31-11-163.us-east-2.compute.internal>
2018-02-18 19:56:40 +0000

    update

--- instructions.R
+++ instructions.R
@@ -2,6 +2,7 @@
 # sudo docker run --rm -p 8000:8000 -v `pwd`/plumber.R:/plumber.R dockerfile /plumber.R
 # sudo docker container ls
 # sudo docker stop fc8f265f279bfc8f265f279b
+# sudo docker container exec -it elegant_austin bash # jump inside running container
 
 campaignPacing
 advertiserPacing
