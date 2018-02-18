27b4cc3aea7a7cd432bdffe0a5910b330323e9f5
Freddy <rstudio@ip-172-31-11-163.us-east-2.compute.internal>
2018-02-18 23:27:34 +0000

    simple

--- instructions.R
+++ instructions.R
@@ -2,6 +2,7 @@
 # sudo docker run --rm -p 8000:8000 -v `pwd`/plumber.R:/plumber.R dockerfile /plumber.R
 # sudo docker container ls
 # sudo docker stop fc8f265f279bfc8f265f279b
+# sudo siege 52.14.83.204/mean?n=1000 -b -t30s
 
 campaignPacing
 advertiserPacing
